c0b2ad0a8435ed34d4f17c8d380fc5ad71c22984
Artashes Manukyan <artashes.manukyan@stud.tu-darmstadt.de>
2023-03-05 19:32:46 +0100

    verarbeitete(mit benoetigte Zahlen) Aufgabe 1.b

--- pa-ws2223-stud/main.py
+++ pa-ws2223-stud/main.py
@@ -1,6 +1,10 @@
 def main():
     # setting hdf5 file's path (with name)
     data_file = "../project/data/data_230209_GdD_PA3_Datensatz_Kennfeldmessung_Exzenterschneckenpumpe_Sirupmischanlage_Logan.hdf5"
+    #setting array which keeps speeds
+    speeds = ['540','1400','300']
+    #setting array which keeps indexes
+    index = ['deg16.5' , 'deg18' , 'deg24' , 'deg36' , 'deg60']
     pass
 
 
