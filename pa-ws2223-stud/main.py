c02281739212006b1f8ec88a0f578c0152fc42cd
Artashes Manukyan <artashes.manukyan@stud.tu-darmstadt.de>
2023-03-05 19:36:48 +0100

    verarbeitete Aufgabe 1.c

--- pa-ws2223-stud/main.py
+++ pa-ws2223-stud/main.py
@@ -5,6 +5,8 @@ def main():
     speeds = ['540','1400','300']
     #setting array which keeps indexes
     index = ['deg16.5' , 'deg18' , 'deg24' , 'deg36' , 'deg60']
+    #setting array with features of interest
+    features_of_interest=['cyan_dp','cyan_q']
     pass
 
 
